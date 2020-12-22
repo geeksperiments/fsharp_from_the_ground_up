591b9c5f4d3d70cbdc9baa28270ff7c8b65277c6
Paul Amazona <whatevergeek@gmail.com>
2020-12-22 22:00:32 +0800

    if else branch

--- 11_ConditionalBranchingWithIfElse/MyCode/helloworld/Program.fs
+++ 11_ConditionalBranchingWithIfElse/MyCode/helloworld/Program.fs
@@ -5,9 +5,10 @@ open System
 [<EntryPoint>]
 let main argv =
     let person = 
-        match argv with
-        | arg when arg.Length > 0 -> arg.[0]
-        | _ -> "Anonymous Person"
+        if argv.Length > 0 then 
+            argv.[0]
+        else
+            "Anonymous Person"
 
     printfn $"Hello {person} from my F# program!"
     0 // return an integer exit code
