5f683122f50ca617f81c41db6e007d940205dd85
Paul Amazona <whatevergeek@gmail.com>
2021-08-01 18:59:47 +0800

    chapter 47: added solution to ex. 1

---
+++ 47_ObjectOrientationAndClasses/MyCode/Classes/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Classes
+
+[<EntryPoint>]
+let main argv =
+    let namePrompt = ConsolePrompt("Please enter your name", 3)
+    namePrompt.BeepOnError <- false
+    namePrompt.ColorScheme <- ConsoleColor.Cyan, ConsoleColor.Cyan
+    printfn $"Hello %s{namePrompt.GetValue()}"
+    0 // return an integer exit code
